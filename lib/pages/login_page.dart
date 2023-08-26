9abc58d681eead539d769b712c58fcdd738aaf7e
Gustavo Giannini <gustavo-giannini@hotmail.com>
2023-08-26 20:15:54 -0300

    Correção scroll automático

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,102 +15,106 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
+          //Adicionado para parar de scrollar o fundo junto
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const SizedBox(height: 50),
 
-              // logo
-              const Icon(
-                Icons.lock,
-                size: 100,
-              ),
-
-              const SizedBox(height: 50),
-
-              // welcome back, you've been missed!
-              Text(
-                'Bem vindo!',
-                style: TextStyle(
-                  color: Colors.grey[700],
-                  fontSize: 16,
+                // logo
+                const Icon(
+                  Icons.lock,
+                  size: 100,
                 ),
-              ),
 
-              const SizedBox(height: 25),
+                const SizedBox(height: 50),
 
-              // username textfield
-              campoInput(
-                hintText: 'Email',
-                obscureText: false,
-              ),
+                // welcome back, you've been missed!
+                Text(
+                  'Bem vindo!',
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16,
+                  ),
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 25),
 
-              // password textfield
-              campoInput(
-                hintText: 'Senha',
-                obscureText: true,
-              ),
+                // username textfield
+                campoInput(
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              const SizedBox(height: 25),
+                // password textfield
+                campoInput(
+                  hintText: 'Senha',
+                  obscureText: true,
+                ),
 
-              // // sign in button
-              // MyButton(
-              //   onTap: signUserIn,
-              // ),
+                const SizedBox(height: 10),
 
-              const SizedBox(height: 50),
+                const SizedBox(height: 25),
 
-              // or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
+                // // sign in button
+                // MyButton(
+                //   onTap: signUserIn,
+                // ),
+
+                const SizedBox(height: 50),
+
+                // or continue with
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                // not a member? register now
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
+                    Text(
+                      'Não possuí conta?',
+                      style: TextStyle(color: Colors.grey[700]),
                     ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
+                    const SizedBox(width: 4),
+                    InkWell(
+                      child: const Text(
+                        'Cadastrar-se',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
+                      onTap: () {
+                        Navigator.pushNamed(context, '/cadastro');
+                      },
                     ),
                   ],
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              // not a member? register now
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Não possuí conta?',
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                  const SizedBox(width: 4),
-                  InkWell(
-                    child: const Text(
-                      'Cadastrar-se',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    onTap: () {
-                      Navigator.pushNamed(context, '/cadastro');
-                    },
-                  ),
-                ],
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
